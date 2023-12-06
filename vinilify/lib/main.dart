9a4b6d5523a8f9e2bbf0545e0e87e4be0e9687ad
borotalcohol <loschiavo.christian@gmail.com>
2023-12-06 18:07:50 +0100

    Removed Debug Banner

--- vinilify/lib/main.dart
+++ vinilify/lib/main.dart
@@ -14,6 +14,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Vinilify',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         primaryColor: const Color(0xFF272727),
         textTheme: const TextTheme(
